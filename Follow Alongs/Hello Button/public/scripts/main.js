ef972489ef113b54fd2a6264d3f60ddac884e033
David Fisher <fisherds@rose-hulman.edu>
2020-08-19 12:15:20 -0400

    javascript interactions

--- Follow Alongs/Hello Button/public/scripts/main.js
+++ Follow Alongs/Hello Button/public/scripts/main.js
@@ -1,5 +1,25 @@
+var counter = 0;
+
 function main() {
   console.log("Ready");
+
+  document.querySelector("#decButton").onclick = (event) => {
+    counter = counter - 1;
+    updateView();
+  };
+  document.querySelector("#resetButton").onclick = (event) => {
+    counter = 0;
+    updateView();
+  };
+  document.querySelector("#incButton").onclick = (event) => {
+    counter = counter + 1;
+    updateView();
+  };
+}
+
+function updateView() {
+  // document.querySelector("#counterText").innerHTML = "Count = " + counter;
+  document.querySelector("#counterText").innerHTML = `Count = ${counter}`;
 }
 
 
