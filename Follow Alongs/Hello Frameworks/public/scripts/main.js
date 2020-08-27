dd4e0f54ffeab707058859ef436737c13c7e80ab
David Fisher <fisherds@rose-hulman.edu>
2020-08-27 11:19:19 -0400

    for loops

--- Follow Alongs/Hello Frameworks/public/scripts/main.js
+++ Follow Alongs/Hello Frameworks/public/scripts/main.js
@@ -1,36 +1,34 @@
-/**
- * @fileoverview
- * Provides the JavaScript interactions for all pages.
- *
- * @author 
- * PUT_YOUR_NAME_HERE
- */
-
-/** namespace. */
 var rhit = rhit || {};
 
-/** globals */
-rhit.variableName = "";
+rhit.counter = 0;
 
-/** function and class syntax examples */
-rhit.functionName = function () {
-	/** function body */
-};
 
-rhit.ClassName = class {
-	constructor() {
-
-	}
-
-	methodName() {
-
-	}
-}
-
-/* Main */
-/** function and class syntax examples */
 rhit.main = function () {
 	console.log("Ready");
+	const buttons = document.querySelectorAll("#counterButtons button");
+	// for (let i = 0; i < buttons.length; i++) {
+	// 	const button = buttons[i];
+	// 	button.onclick = (event) => {
+	// 		console.log(`You pressed`, button);
+	// 	};
+	// }
+
+	for (const button of buttons) {
+		button.onclick = (event) => {
+			console.log(`You pressed`, button);
+		};
+	}
+
+	// buttons.forEach((button) => {
+	// 	button.onclick = (event) => {
+	// 		console.log(`You pressed`, button);
+	// 	};
+	// });
+
+};
+
+rhit.updateCounter = function () {
+	
 };
 
 rhit.main();
