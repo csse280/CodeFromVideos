9626f5162ca301d48b1bf8cf3ca4068da702b8ae
David Fisher <fisherds@rose-hulman.edu>
2020-08-27 12:28:01 -0400

    convert JS to use jQuery

--- Follow Alongs/Hello Frameworks/public/scripts/main.js
+++ Follow Alongs/Hello Frameworks/public/scripts/main.js
@@ -5,7 +5,8 @@ rhit.counter = 0;
 
 rhit.main = function () {
 	console.log("Ready");
-	const buttons = document.querySelectorAll("#counterButtons button");
+	// const buttons = document.querySelectorAll("#counterButtons button");
+
 	// for (let i = 0; i < buttons.length; i++) {
 	// 	const button = buttons[i];
 	// 	button.onclick = (event) => {
@@ -13,20 +14,27 @@ rhit.main = function () {
 	// 	};
 	// }
 
-	for (const button of buttons) {
-		button.onclick = (event) => {
-			const dataAmount = parseInt(button.dataset.amount);
-			const dataIsMultiplication = button.dataset.isMultiplication == "true";
-			// console.log(`Amount: ${dataAmount} isMult: ${dataIsMultiplication}`);
-			// console.log(`Amount: ${typeof(dataAmount)} isMult: ${typeof(dataIsMultiplication)}`);
-			rhit.updateCounter(dataAmount, dataIsMultiplication);
-		};
-	}
+	// for (const button of buttons) {
+	// 	button.onclick = (event) => {
+	// 		const dataAmount = parseInt(button.dataset.amount);
+	// 		const dataIsMultiplication = button.dataset.isMultiplication == "true";
+	// 		// console.log(`Amount: ${dataAmount} isMult: ${dataIsMultiplication}`);
+	// 		// console.log(`Amount: ${typeof(dataAmount)} isMult: ${typeof(dataIsMultiplication)}`);
+	// 		rhit.updateCounter(dataAmount, dataIsMultiplication);
+	// 	};
+	// }
+
 	// buttons.forEach((button) => {
 	// 	button.onclick = (event) => {
 	// 		console.log(`You pressed`, button);
 	// 	};
 	// });
+
+	$("#counterButtons button").click((event) => {
+		const dataAmount = $(event.target).data("amount");
+		const dataIsMultiplication = !!$(event.target).data("isMultiplication");
+		rhit.updateCounter(dataAmount, dataIsMultiplication);
+	});
 };
 
 rhit.updateCounter = function (amount, isMultiplication) {
@@ -35,7 +43,8 @@ rhit.updateCounter = function (amount, isMultiplication) {
 	} else {
 		rhit.counter = rhit.counter + amount;
 	}
-	document.querySelector("#counterText").innerHTML = `Count = ${rhit.counter}`;
+	// document.querySelector("#counterText").innerHTML = `Count = ${rhit.counter}`;
+	$("#counterText").html(`Count = ${rhit.counter}`);
 };
 
 rhit.main();
