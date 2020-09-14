cbcf8facee6a447356f3f82ee11f24ff2314351a
David Fisher <fisherds@rose-hulman.edu>
2020-09-14 12:04:22 -0400

    instance variables

--- Follow Alongs/TicTacToe/public/scripts/main.js
+++ Follow Alongs/TicTacToe/public/scripts/main.js
@@ -2,7 +2,7 @@ var rhit = rhit || {};
 
 rhit.PageController = class {
 	constructor() {
-		// Enable the onclick listeners
+		this.game = new rhit.Game();
 	}
 
 	updateView() {
@@ -11,8 +11,29 @@ rhit.PageController = class {
 };
 
 rhit.Game = class {
+
+	static Mark = {
+		X: "X",
+		O: "O",
+		NONE: " "
+	}
+
+	static State = {
+		X_TURN: "X's Turn",
+		O_TURN: "O's Turn",
+		X_WIN: "X Wins!",
+		O_WIN: "O Wins!",
+		TIE: "Tie Game"
+	}
+
 	constructor() {
-		// TODO: Make instance variables
+		this.state = rhit.Game.State.X_TURN;
+		this.board = [];
+		for (let k = 0; k < 9; k++) {
+			this.board.push(rhit.Game.Mark.NONE);
+		}
+		console.log('this.board = ', this.board);
+		console.log('this.state = ', this.state);
 	}
 
 	pressedButtonAtIndex(buttonIndex) {
