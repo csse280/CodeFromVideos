3e21f6963e51757c3a995e82f8d5f0d3fa3f6c54
David Fisher <fisherds@rose-hulman.edu>
2020-09-25 15:35:36 -0400

    JavaScript scaffolding

--- Follow Alongs/MovieQuotesNoAuth/public/scripts/main.js
+++ Follow Alongs/MovieQuotesNoAuth/public/scripts/main.js
@@ -1,36 +1,60 @@
-/**
- * @fileoverview
- * Provides the JavaScript interactions for all pages.
- *
- * @author 
- * PUT_YOUR_NAME_HERE
- */
-
-/** namespace. */
 var rhit = rhit || {};
 
-/** globals */
-rhit.variableName = "";
+rhit.FB_COLLECTION_MOVIEQUOTE = "MovieQuotes";
+rhit.FB_KEY_QUOTE = "quote";
+rhit.FB_KEY_MOVIE = "movie";
+rhit.FB_KEY_LAST_TOUCHED = "lastTouched";
+rhit.fbMovieQuoteManager = null;
 
-/** function and class syntax examples */
-rhit.functionName = function () {
-	/** function body */
-};
 
-rhit.ClassName = class {
+rhit.ListPageController = class {
 	constructor() {
-
+		console.log("created ListPageController");
 	}
+	updateList() {}
+}
 
-	methodName() {
-
+rhit.MovieQuote = class {
+	constructor(id, quote, movie) {
+		this.id = id;
+		this.quote = quote;
+		this.movie = movie;
 	}
 }
+
+rhit.FbMovieQuotesManager = class {
+	constructor() {
+		console.log("created FbMovieQuotesManager");
+		this._documentSnapshots = [];
+		this._ref = firebase.firestore().collection(rhit.FB_COLLECTION_MOVIEQUOTE);
+	}
+	add(quote, movie) {}
+	beginListening(changeListener) {}
+	stopListening() {}
+	// update(id, quote, movie) {}
+	// delete(id) {}
+	get length() {}
+	getMovieQuoteAtIndex(index) {}
+}
+
 
 /* Main */
 /** function and class syntax examples */
 rhit.main = function () {
 	console.log("Ready");
+
+	if (document.querySelector("#listPage")) {
+		console.log("You are on the list page.");
+		rhit.fbMovieQuoteManager = new rhit.FbMovieQuotesManager();
+		new rhit.ListPageController();
+	}
+
+
+	if (document.querySelector("#detailPage")) {
+		console.log("You are on the detail page.");
+
+
+	}
 
 
 	// Temp code for Read and Add
