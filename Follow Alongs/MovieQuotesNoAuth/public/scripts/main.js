8265f5dc675bdc7ad366edb64eae993075b76dac
David Fisher <fisherds@rose-hulman.edu>
2020-09-29 10:41:47 -0400

    programmatically create a list view

--- Follow Alongs/MovieQuotesNoAuth/public/scripts/main.js
+++ Follow Alongs/MovieQuotesNoAuth/public/scripts/main.js
@@ -40,6 +40,7 @@ rhit.ListPageController = class {
 
 	}
 
+
 	updateList() {
 		console.log("I need to update the list on the page!");
 		console.log(`Num quotes = ${rhit.fbMovieQuotesManager.length}`);
@@ -48,6 +49,11 @@ rhit.ListPageController = class {
 		// Make a new quoteListContainer
 		const newList = htmlToElement('<div id="quoteListContainer"></div>');
 		// Fill the quoteListContainer with quote cards using a loop
+		for (let i = 0; i < rhit.fbMovieQuotesManager.length; i++) {
+			const mq = rhit.fbMovieQuotesManager.getMovieQuoteAtIndex(i);
+			const newCard = this._createCard(mq);
+			newList.appendChild(newCard);
+		}
 
 
 		// Remove the old quoteListContainer
@@ -57,6 +63,16 @@ rhit.ListPageController = class {
 		// Put in the new quoteListContainer
 		oldList.parentElement.appendChild(newList);
 	}
+
+	_createCard(movieQuote) {
+		return htmlToElement(`<div class="card">
+		<div class="card-body">
+			<h5 class="card-title">${movieQuote.quote}</h5>
+			<h6 class="card-subtitle mb-2 text-muted">${movieQuote.movie}</h6>
+		</div>
+	</div>`);
+	}
+
 }
 
 rhit.MovieQuote = class {
