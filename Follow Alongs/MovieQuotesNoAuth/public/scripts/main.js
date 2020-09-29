ebe09287cabf698596bb105bee47f2e12b1fb9a3
David Fisher <fisherds@rose-hulman.edu>
2020-09-28 23:21:48 -0400

    removing the first create video attemp

--- Follow Alongs/MovieQuotesNoAuth/public/scripts/main.js
+++ Follow Alongs/MovieQuotesNoAuth/public/scripts/main.js
@@ -9,25 +9,6 @@ rhit.fbMovieQuotesManager = null;
 
 rhit.ListPageController = class {
 	constructor() {
-
-		document.querySelector("#submitAddQuote").onclick = (event) => {
-			const quote = document.querySelector("#inputQuote").value;
-			const movie = document.querySelector("#inputMovie").value;
-			console.log(quote, movie);
-			rhit.fbMovieQuotesManager.add(quote, movie);
-		};
-		$("#addQuoteDialog").on("show.bs.modal", (event) => {
-			// Pre animation
-			document.querySelector("#inputQuote").value = "";
-			document.querySelector("#inputMovie").value = "";
-		});
-
-		$("#addQuoteDialog").on("shown.bs.modal", (event) => {
-			// Post animation
-			document.querySelector("#inputQuote").focus();
-		});
-
-
 	}
 	updateList() {}
 }
@@ -48,17 +29,6 @@ rhit.FbMovieQuotesManager = class {
 	}
 
 	add(quote, movie) {
-		this._ref.add({
-				[rhit.FB_KEY_QUOTE]: quote,
-				[rhit.FB_KEY_MOVIE]: movie,
-				[rhit.FB_KEY_LAST_TOUCHED]: firebase.firestore.Timestamp.now(),
-			})
-			.then((docRef) => {
-				console.log("Document written with ID: ", docRef.id);
-			})
-			.catch((error) => {
-				console.error("Error adding document: ", error);
-			});
 	}
 
 	beginListening(changeListener) {}
@@ -74,20 +44,15 @@ rhit.FbMovieQuotesManager = class {
 /** function and class syntax examples */
 rhit.main = function () {
 	console.log("Ready");
-
 	if (document.querySelector("#listPage")) {
 		console.log("You are on the list page.");
 		rhit.fbMovieQuotesManager = new rhit.FbMovieQuotesManager();
 		new rhit.ListPageController();
 	}
-
-
 	if (document.querySelector("#detailPage")) {
 		console.log("You are on the detail page.");
 
-
 	}
-
 
 	// Temp code for Read and Add
 	// const ref = firebase.firestore().collection("MovieQuotes");
