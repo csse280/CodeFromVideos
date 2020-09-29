ab61cf4d7c4b9f4067b97739a2920a01acf0eccb
David Fisher <fisherds@rose-hulman.edu>
2020-09-29 11:31:04 -0400

    session store to save the moviequote id

--- Follow Alongs/MovieQuotesNoAuth/public/scripts/main.js
+++ Follow Alongs/MovieQuotesNoAuth/public/scripts/main.js
@@ -52,6 +52,10 @@ rhit.ListPageController = class {
 		for (let i = 0; i < rhit.fbMovieQuotesManager.length; i++) {
 			const mq = rhit.fbMovieQuotesManager.getMovieQuoteAtIndex(i);
 			const newCard = this._createCard(mq);
+			newCard.onclick = (event) => {
+				console.log(`You clicked on ${mq.id}`);
+				rhit.storage.setMovieQuoteId(mq.id);
+			};
 			newList.appendChild(newCard);
 		}
 
@@ -139,6 +143,20 @@ rhit.FbMovieQuotesManager = class {
 	}
 }
 
+rhit.storage = rhit.storage || {};
+rhit.storage.MOVIEQUOTE_ID_KEY = "movieQuoteId";
+
+rhit.storage.getMovieQuoteId = function() {
+	const mqId = sessionStorage.getItem(rhit.storage.MOVIEQUOTE_ID_KEY);
+	if (!mqId) {
+		console.log("No movie quote id in sessionStorage!");
+	}
+	return mqId;
+};
+
+rhit.storage.setMovieQuoteId = function(movieQuoteId) {
+	sessionStorage.setItem(rhit.storage.MOVIEQUOTE_ID_KEY, movieQuoteId);
+};
 
 /* Main */
 /** function and class syntax examples */
