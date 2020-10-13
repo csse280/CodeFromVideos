7f79e481e82c6397fdb70489e1e7956c822c9130
David Fisher <fisherds@rose-hulman.edu>
2020-10-13 11:23:41 -0400

    email password html and js

--- Follow Alongs/FirebaseAuth/public/scripts/main.js
+++ Follow Alongs/FirebaseAuth/public/scripts/main.js
@@ -1,36 +1,21 @@
-/**
- * @fileoverview
- * Provides the JavaScript interactions for all pages.
- *
- * @author 
- * PUT_YOUR_NAME_HERE
- */
-
-/** namespace. */
 var rhit = rhit || {};
 
-/** globals */
-rhit.variableName = "";
-
-/** function and class syntax examples */
-rhit.functionName = function () {
-	/** function body */
-};
-
-rhit.ClassName = class {
-	constructor() {
-
-	}
-
-	methodName() {
-
-	}
-}
-
-/* Main */
-/** function and class syntax examples */
 rhit.main = function () {
 	console.log("Ready");
+	const inputEmailEl = document.querySelector("#inputEmail");
+	const inputPasswordEl = document.querySelector("#inputPassword");
+
+	document.querySelector("#signOutButton").onclick = (event) => {
+		console.log(`Sign out`);
+	};
+	document.querySelector("#createAccountButton").onclick = (event) => {
+		console.log(`Create account for email: ${inputEmailEl.value} password:  ${inputPasswordEl.value}`);
+	};
+	document.querySelector("#logInButton").onclick = (event) => {
+		console.log(`Log in for email: ${inputEmailEl.value} password:  ${inputPasswordEl.value}`);
+	
+	};
+
 };
 
 rhit.main();
