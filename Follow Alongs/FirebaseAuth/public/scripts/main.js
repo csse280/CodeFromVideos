c12f22c8e46eb6d35035a773baa5e063af466219
David Fisher <fisherds@rose-hulman.edu>
2020-10-13 12:04:59 -0400

    firebase ui

--- Follow Alongs/FirebaseAuth/public/scripts/main.js
+++ Follow Alongs/FirebaseAuth/public/scripts/main.js
@@ -60,7 +60,21 @@ rhit.main = function () {
 			console.log("Anonymous auth error", errorCode, errorMessage);
 		});
 	};
-
+	rhit.startFirebaseUI();
 };
+
+rhit.startFirebaseUI = function() {
+	var uiConfig = {
+		signInSuccessUrl: '/',
+		signInOptions: [
+			firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+			firebase.auth.EmailAuthProvider.PROVIDER_ID,
+			firebase.auth.PhoneAuthProvider.PROVIDER_ID,
+			firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID
+		],
+	};
+	const ui = new firebaseui.auth.AuthUI(firebase.auth());
+	ui.start('#firebaseui-auth-container', uiConfig);
+}
 
 rhit.main();
