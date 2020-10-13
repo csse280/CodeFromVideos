c35a247f2a9ef67227a23c42ea44b48ff1f713a7
David Fisher <fisherds@rose-hulman.edu>
2020-10-13 11:38:38 -0400

    email password auth

--- Follow Alongs/FirebaseAuth/public/scripts/main.js
+++ Follow Alongs/FirebaseAuth/public/scripts/main.js
@@ -2,20 +2,51 @@ var rhit = rhit || {};
 
 rhit.main = function () {
 	console.log("Ready");
+
+	firebase.auth().onAuthStateChanged((user) => {
+		if (user) {
+			var displayName = user.displayName;
+			var email = user.email;
+			var emailVerified = user.emailVerified;
+			var photoURL = user.photoURL;
+			var isAnonymous = user.isAnonymous;
+			var uid = user.uid;
+			var providerData = user.providerData;
+			
+			console.log("The user is signed in ", uid);
+		} else {
+			console.log("There is no user signed in!");
+		}
+	});
+
 	const inputEmailEl = document.querySelector("#inputEmail");
 	const inputPasswordEl = document.querySelector("#inputPassword");
 
 	document.querySelector("#signOutButton").onclick = (event) => {
 		console.log(`Sign out`);
+		firebase.auth().signOut().then(function () {
+			console.log("You are now signed out");
+		}).catch(function (error) {
+			console.log("Sign out error");
+		});
 	};
+
 	document.querySelector("#createAccountButton").onclick = (event) => {
 		console.log(`Create account for email: ${inputEmailEl.value} password:  ${inputPasswordEl.value}`);
+		firebase.auth().createUserWithEmailAndPassword(inputEmailEl.value, inputPasswordEl.value).catch(function (error) {
+			var errorCode = error.code;
+			var errorMessage = error.message;
+			console.log("Create account error", errorCode, errorMessage);
+		});
 	};
 	document.querySelector("#logInButton").onclick = (event) => {
 		console.log(`Log in for email: ${inputEmailEl.value} password:  ${inputPasswordEl.value}`);
-	
+		firebase.auth().signInWithEmailAndPassword(inputEmailEl.value, inputPasswordEl.value).catch(function (error) {
+			var errorCode = error.code;
+			var errorMessage = error.message;
+			console.log("Existing account log in error", errorCode, errorMessage);
+		});
 	};
-
 };
 
 rhit.main();
