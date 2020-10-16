0cf54dfa322b2365ebf76ddf7f78392c154ac87d
David Fisher <fisherds@rose-hulman.edu>
2020-10-16 09:21:06 -0400

    authorizations for the detail page

--- Follow Alongs/MyMovieQuotes/public/scripts/main.js
+++ Follow Alongs/MyMovieQuotes/public/scripts/main.js
@@ -162,7 +162,9 @@ rhit.FbMovieQuotesManager = class {
 
 rhit.DetailPageController = class {
 	constructor() {
-
+		document.querySelector("#menuSignOut").addEventListener("click", (event) => {
+			rhit.fbAuthManager.signOut();
+		});
 		document.querySelector("#submitEditQuote").addEventListener("click", (event) => {
 			const quote = document.querySelector("#inputQuote").value;
 			const movie = document.querySelector("#inputMovie").value;
@@ -182,7 +184,7 @@ rhit.DetailPageController = class {
 		document.querySelector("#submitDeleteQuote").addEventListener("click", (event) => {
 			rhit.fbSingleQuoteManager.delete().then(function () {
 				console.log("Document successfully deleted!");
-				window.location.href = "/";
+				window.location.href = "/list.html";
 			}).catch(function (error) {
 				console.error("Error removing document: ", error);
 			});
@@ -193,6 +195,10 @@ rhit.DetailPageController = class {
 	updateView() {
 		document.querySelector("#cardQuote").innerHTML = rhit.fbSingleQuoteManager.quote;
 		document.querySelector("#cardMovie").innerHTML = rhit.fbSingleQuoteManager.movie;
+		if (rhit.fbSingleQuoteManager.author == rhit.fbAuthManager.uid) {
+			document.querySelector("#menuEdit").style.display = "flex";
+			document.querySelector("#menuDelete").style.display = "flex";
+		}
 	}
 }
 
@@ -246,6 +252,10 @@ rhit.FbSingleQuoteManager = class {
 
 	get movie() {
 		return this._documentSnapshot.get(rhit.FB_KEY_MOVIE);
+	}
+
+	get author() {
+		return this._documentSnapshot.get(rhit.FB_KEY_AUTHOR);
 	}
 }
 
