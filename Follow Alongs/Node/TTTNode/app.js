e409a1de3bfb1e486a49ee359c7cd6df8d492dd2
David Fisher <fisherds@rose-hulman.edu>
2020-11-03 11:12:21 -0500

    simple TTT via Node

---
+++ Follow Alongs/Node/TTTNode/app.js
@@ -0,0 +1,5 @@
+const express = require("express");
+
+var app = express();
+app.use(express.static("public"));
+app.listen(3000);
---
+++ Follow Alongs/Node/TTTNode/public/scripts/main.js
@@ -0,0 +1,124 @@
+var rhit = rhit || {};
+
+rhit.PageController = class {
+	constructor() {
+		this.game = new rhit.Game();
+
+		const squares = document.querySelectorAll(".square");
+		for (const square of squares) {
+			square.onclick = (event) => {
+				const buttonIndex = parseInt(square.dataset.buttonIndex);
+				this.game.pressedButtonAtIndex(buttonIndex);
+				this.updateView();
+			};
+		}
+		document.querySelector("#newGameButton").onclick = (event) => {
+			this.game = new rhit.Game();
+			this.updateView();
+		};
+		this.updateView();
+	}
+
+	updateView() {
+		const squares = document.querySelectorAll(".square");
+		squares.forEach((square, index) => {
+			square.innerHTML = this.game.getMarkAtIndex(index);
+		});
+		document.querySelector("#gameStateText").innerHTML = this.game.state;
+	}
+};
+
+rhit.Game = class {
+
+	static Mark = {
+		X: "X",
+		O: "O",
+		NONE: " "
+	}
+
+	static State = {
+		X_TURN: "X's Turn",
+		O_TURN: "O's Turn",
+		X_WIN: "X Wins!",
+		O_WIN: "O Wins!",
+		TIE: "Tie Game"
+	}
+
+	constructor() {
+		this.state = rhit.Game.State.X_TURN;
+		this.board = [];
+		for (let k = 0; k < 9; k++) {
+			this.board.push(rhit.Game.Mark.NONE);
+		}
+	}
+
+	pressedButtonAtIndex(buttonIndex) {
+		if (this.state == rhit.Game.State.X_WIN ||
+			  this.state == rhit.Game.State.O_WIN ||
+			  this.state == rhit.Game.State.TIE) {
+					console.log("The game is over");
+			return;
+		}
+		if (this.board[buttonIndex] != rhit.Game.Mark.NONE) {
+			console.log("The square is not empty");
+			return;
+		}
+		if (this.state == rhit.Game.State.X_TURN) {
+			this.board[buttonIndex] = rhit.Game.Mark.X;
+			this.state = rhit.Game.State.O_TURN;
+		} else {
+			this.board[buttonIndex] = rhit.Game.Mark.O;
+			this.state = rhit.Game.State.X_TURN;
+		}
+		this._checkForGameOver();
+	}
+
+	_checkForGameOver() {
+		// Check for a tie game
+		if (!this.board.includes(rhit.Game.Mark.NONE)) {
+			this.state = rhit.Game.State.TIE;
+		}
+		const linesOf3 = [];
+		linesOf3.push(this.board[0] + this.board[1] + this.board[2]);
+		linesOf3.push(this.board[3] + this.board[4] + this.board[5]);
+		linesOf3.push(this.board[6] + this.board[7] + this.board[8]);
+
+		linesOf3.push(this.board[0] + this.board[3] + this.board[6]);
+		linesOf3.push(this.board[1] + this.board[4] + this.board[7]);
+		linesOf3.push(this.board[2] + this.board[5] + this.board[8]);
+
+		linesOf3.push(this.board[0] + this.board[4] + this.board[8]);
+		linesOf3.push(this.board[2] + this.board[4] + this.board[6]);
+
+		for (const lineOf3 of linesOf3) {
+			if (lineOf3 == "XXX") {
+				this.state = rhit.Game.State.X_WIN;
+			} else if (lineOf3 == "OOO") {
+				this.state = rhit.Game.State.O_WIN;
+			}
+		}
+	}
+
+	getMarkAtIndex(buttonIndex) {
+		return this.board[buttonIndex];
+	}
+
+	getState() {
+		return this.state;
+	}
+};
+
+rhit.main = function () {
+	console.log("Ready");
+	new rhit.PageController();
+
+	// Note: you could develop the model first doing stuff like this...
+	// const myGame = new rhit.Game();
+	// console.log('this.board = ', myGame.board);
+	// console.log('this.state = ', myGame.state);
+	// myGame.pressedButtonAtIndex(4);
+	// console.log('this.board = ', myGame.board);
+	// console.log('this.state = ', myGame.state);
+};
+
+rhit.main();
