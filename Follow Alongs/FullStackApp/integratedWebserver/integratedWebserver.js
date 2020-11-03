f59667bed15a2610ae47313a9f2657f95c0a1589
Jason Yoder <yoder1@rose-hulman.edu>
2020-11-03 09:18:15 -0500

    basic frontend code setup

---
+++ Follow Alongs/FullStackApp/integratedWebserver/integratedWebserver.js
@@ -0,0 +1,42 @@
+var express = require("express");
+var app = express();
+const logger = require("morgan"); //useful for logging requests
+app.use(logger("dev") );
+
+let data=[];
+let counter = 0;
+
+const fs = require("fs");
+const serverSideStorage = "../data/db.json";
+
+fs.readFile(serverSideStorage, function(err, buf) {
+    if (err) {
+        console.log("error: ", err);
+    } else {
+        data = JSON.parse( buf.toString()  );
+        if (data.length != 0) {
+            counter = data[data.length-1];
+        }
+    }
+    console.log("Data read from file.");
+});
+
+function saveToServer(data) {
+    fs.writeFile(serverSideStorage, JSON.stringify(data), function(err, buf) {
+        if (err) {
+            console.log("error: ", err);
+        } else {
+            console.log("Data saved successfully!");
+        }
+    })
+}
+
+app.use('/static', express.static("public") );
+
+
+//middleware
+var bodyParser = require("body-parser");
+app.use('/api/', bodyParser.urlencoded( {extended: true} )   );
+app.use('/api/', bodyParser.json()  );
+
+app.listen(3000);
