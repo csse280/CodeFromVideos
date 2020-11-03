9aec4eb1162894cde81bfc030581d51dd6fb046d
David Fisher <fisherds@rose-hulman.edu>
2020-11-03 11:22:31 -0500

    Node server API complete

--- Follow Alongs/Node/TTTNode/app.js
+++ Follow Alongs/Node/TTTNode/app.js
@@ -2,4 +2,25 @@ const express = require("express");
 
 var app = express();
 app.use(express.static("public"));
+
+
+app.get("/api/getmove/:board", (request, response) => {
+  const boardString = request.params.board;
+  const openings = getOpenLocations(boardString);
+  const moveSelected = openings[Math.floor(Math.random() * openings.length)];
+  response.send({"move": moveSelected});
+});
+
+
+function getOpenLocations(boardString) {
+  const openLocations = [];
+  for (var i = 0; i < boardString.length; i++) {
+    if (boardString.charAt(i) == '-') {
+      openLocations.push(i)
+    }
+  }
+  return openLocations;
+ }
+
+
 app.listen(3000);
